7d6ab58effe061631167a0cc6c1699c2689a2589
Imrul Hassaan Emon <imrulhassan989@gmail.com>
2025-08-20 01:57:47 +0600

    added ResuableFrom

--- src/App.jsx
+++ src/App.jsx
@@ -1,16 +1,35 @@
 import './App.css'
 import SimpleComponent from './Component/simpleComponent/SimpleComponent'
 import StatefulForm from '../src/Component/staateFul/statefulForm'
-import RefForm from './Component/RefForm/RefForm'
+import ResuableForm from './Component/ResuableForm/ResuableForm'
+// import RefForm from './Component/RefForm/RefForm'
 
 function App() {
+
+  const handleSignupSubmit = data => {
+      console.log('signUp', data)
+
+    }
+
+  const handleUpdateProfail = data => {
+    console.log('updateProfail', data)
+
+  }
 
   return (
     <>
       <h1>Vite + React</h1>
      {/* <SimpleComponent></SimpleComponent> */}
      {/* <StatefulForm></StatefulForm> */}
-     <RefForm></RefForm>
+     {/* <RefForm></RefForm> */}
+     <ResuableForm formTitle={'Form Title'} handleSubmit={handleSignupSubmit} >
+      <h1>Sign Up</h1>
+      <p>Please SignUp Right Now</p>
+     </ResuableForm>
+     <ResuableForm formTitle={'Profile Update'} handleSubmit={handleUpdateProfail} submitBtnText="Update">
+      <h1>Update Profail</h1>
+      <p>Please Update Your Profile</p>
+     </ResuableForm>
     </>
   )
 }
---
+++ src/Component/ResuableForm/ResuableForm.jsx
@@ -0,0 +1,31 @@
+import React from 'react';
+
+const ResuableForm = ({submitBtnText = 'Submit',  handleSubmit, children}) => {
+
+    const handleLocalSubmit = e => {
+        e.preventDefault()
+        const data = {
+            name: e.target.name.value,
+            email: e.target.email.value,
+            password: e.target.password.value
+        }
+        handleSubmit(data)
+    }
+
+    return (
+        <div >
+            <p>{children}</p>
+            <form onSubmit={handleLocalSubmit} >
+                <input type="text" name='name'  placeholder="Enter text" />
+                <br />
+                <input type="email"name ='email' placeholder="Enter text" />
+                <br />
+                <input type="password"name ='password' placeholder="Enter text" />
+                <br />
+                <input value={submitBtnText} type="submit"></input>
+            </form>
+        </div>
+    );
+};
+
+export default ResuableForm;
